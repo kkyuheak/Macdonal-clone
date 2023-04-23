4c2c15a377fb369be585c1861050e4bf015fe31b
kkyuheak <gyuheak@gmail.com>
2023-04-23 10:06:29 +0900

    스크롤시 헤더 영역 축소 완성

--- js/main.js
+++ js/main.js
@@ -36,3 +36,24 @@ hiddenMenu.addEventListener("mouseout", () => {
   hiddenMenu.style.display = "none";
   header.style.borderColor = "#ffbc0d";
 });
+
+//스크롤시 헤더 영역 축소
+const headerLogo = document.querySelector(".header_logo");
+const hiddenMenuList = document.querySelector(".hidden_menu-list");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 130) {
+    header.style.height = "80px";
+    headerLogo.style.width = "51px";
+    headerLogo.style.height = "45px";
+    headerLogo.style.transition = "1s";
+    hiddenMenu.style.top = "60px";
+    hiddenMenuList.style.right = "265px";
+  } else {
+    header.style.height = "140px";
+    headerLogo.style.width = "106px";
+    headerLogo.style.height = "93px";
+    hiddenMenu.style.top = "140px";
+    hiddenMenuList.style.right = "165px";
+  }
+});
